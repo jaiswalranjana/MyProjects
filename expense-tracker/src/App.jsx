614571d7c97ae5aa0be8a807d7cf348d365148ef
jaiswalranjana <ranjana.25b01010698@abes.ac.in>
2026-07-02 14:08:27 +0530

    phase 1 done

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -5,11 +5,49 @@ import BudgetSummary from './components/BudgetSummary'
 import SetBudget from './components/SetBudget'
 import AddExpenseForm from './components/AddExpenseForm'
 import MonthlyTransactions from './components/MonthlyTransactions'
+import { useEffect } from 'react'
 
 function App() {
-  const [budget,setBudget]=useState(0);
-  const[expenses,setExpenses]=useState([]);
+  const [budget,setBudget]=useState(
+    () => {
+    const savedBudget = localStorage.getItem("budget");
 
+    if (savedBudget) {
+        return Number(savedBudget);
+    }
+
+    return 0;
+}
+  );
+
+  const[expenses,setExpenses]=useState(()=>{
+    const savedExpenses=localStorage.getItem("expenses");
+
+    if(savedExpenses){
+       return JSON.parse(savedExpenses);
+    }
+   return []
+  }
+  );
+
+
+  useEffect(() => {
+    localStorage.setItem("budget", budget);
+}, [budget]);
+
+  useEffect(()=>{
+    // console.log("useeffect ran ");
+
+    localStorage.setItem(
+        "expenses",
+        JSON.stringify(expenses)
+
+    );
+  },[expenses]);
+
+
+
+  
   return (
     <>
      <Navbar/> 
--- expense-tracker/src/components/MonthlyTransactions.jsx
+++ expense-tracker/src/components/MonthlyTransactions.jsx
@@ -18,8 +18,11 @@ function MonthlyTransactions({expenses,setExpenses}){
         expenses.map((expense)=>(
             <div key={expense.id}>
                <p> {expense.category} -
-                &#8377;{expense.amount}
+                &#8377;{expense.amount} 
             </p>
+            <p>{expense.description}</p>
+            <p>{new Date(expense.date).toLocaleDateString()}</p>
+
             <button onClick={()=>handleDelete(expense.id)}>
                 Delete</button>
             </div>
